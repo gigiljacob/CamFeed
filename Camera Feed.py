6c955e510a7ad2d6a981086d0c632f964d9243af
gigiljacob <33169754+gigiljacob@users.noreply.github.com>
2018-11-15 13:08:11 +0530

    Update Camera Feed.py

--- Camera Feed.py
+++ Camera Feed.py
@@ -1,12 +1,19 @@
+#importing necessary packages
 import cv2
+
 class Cam:
   def startcam(self):
+    #VideoCapture capture your video from source (here 0 is webcam). To take a video file specify filename in quotes 'example.mp4'
     video = cv2.VideoCapture(0)
 
     while True:
+      #reading each frame of the Video obtained from source
       ret, frame = video.read()
+      
+      #displaying captured frame using imshow function
       cv2.imshow('frame', frame)
       
+      #to exit the code
       if cv2.waitKey(1) & 0xFF ==  ord('q'):
         break
     video.release()
